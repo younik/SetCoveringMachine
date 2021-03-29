723bd4a47152cf380d0a4d424252c9dbb645a51d
younik <omar.younis98@gmail.com>
2021-03-29 22:57:38 +0200

    fix long-description

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
-from distutils.core import setup
+from setuptools import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
 setup(
   name='scmpy',
   packages=['scmpy'],
-  version='0.1',
+  version='0.1.2',
   license='MIT',
-  description='The Set Covering Machine is a binary classifier',
+  description='Set Covering Machine is a binary classification algorithm',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
   author='Omar Younis',
   author_email='omar.younis98@gmail.com',
   url='https://github.com/younik',
-  download_url='https://github.com/younik/SetCoveringMachine/archive/refs/tags/v0.1.tar.gz',
+  download_url='https://github.com/younik/SetCoveringMachine/archive/refs/tags/v0.1.2.tar.gz',
   keywords=['SET', 'COVER', 'BINARY', 'CLASSIFICATION', 'MACHINE', 'LEARNING'],
   install_requires=[
           'numpy',
